65767e1d08799a06f38a459288ac513f0329c64b
AthulVinayan <athulvinayan78@gmail.com>
2024-03-18 23:01:18 -0400

    second commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import os
+
+app = Flask(__name__)
+
+# Get the directory of the current script
+# current_dir = os.path.dirname(r"C:\Users\athul\Desktop\Assignments\FishLab4\new")
+
+# Load the trained classification model
+model_path = 'fsp_model.pkl'
+model = joblib.load(model_path)
+
+# Dictionary mapping numeric predictions to species labels
+species_mapping = {
+    0: 'Bream',
+    1: 'Roach',
+    2: 'Whitefish',
+    3: 'Parkki',
+    4: 'Perch',
+    5: 'Pike',
+    6: 'Smelt'
+}
+
+# Route to serve the HTML page
+@app.route('/')
+def index():
+    # Render the HTML template
+    template_path = os.path.join(current_dir, 'templates', 'input.html')
+    return render_template('input.html')
+
+# Route to handle prediction requests
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the input data from the request
+        weight = float(request.form['weight'])
+        length1 = float(request.form['length1'])
+        length2 = float(request.form['length2'])
+        length3 = float(request.form['length3'])
+        height = float(request.form['height'])
+        width = float(request.form['width'])
+        
+
+        # Perform prediction using the model
+        input_data = [[length1, length2, length3, height, width, weight]]
+        prediction = model.predict(input_data)[0]
+        predicted_species = species_mapping[prediction]
+
+        # Return the predicted species
+        return jsonify({'prediction': predicted_species})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run()
